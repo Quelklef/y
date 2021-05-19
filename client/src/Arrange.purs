2e33babd43e1d4111b5f352c48d55eda618b8971
Maynard <elimaynard923@gmail.com>
2021-05-18 23:07:30 -0700

    be slightly less irresponsible with types

--- client/src/Arrange.purs
+++ client/src/Arrange.purs
@@ -2,15 +2,13 @@ module Y.Client.Arrange where
 
 import Prelude
 
-import Debug as Debug
-
 import Data.List (List)
 import Data.List as List
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Tuple.Nested (type (/\), (/\))
 import Data.Maybe (Maybe(..), fromJust, fromMaybe)
-import Data.Foldable (class Foldable, sum, length, foldl, maximum, minimum)
+import Data.Foldable (sum, length, foldl, maximum, minimum)
 import Data.Function (on)
 import Data.Int as Int
 import Partial.Unsafe (unsafePartial)
@@ -41,25 +39,15 @@ calcTiers getId getDeps nodes =
 arrange :: forall node time id. Ord node => Ord time => Ord id =>
            GetId node id -> GetDeps node id -> GetTime node time -> GetDims node ->
            List node -> Map id Vec2
-arrange getId getDeps getTime getDims =
+arrange getId getDeps getTime getDims = unsafePartial $
           stage_1'formativeArrangement
       >>> stage_2'normalization
       >>> stage_3'jostling
 
   where
 
+    average :: forall a. EuclideanRing a => List a -> Maybe a
     average xs = if List.null xs then Nothing else Just $ sum xs / length xs
-
-    -- unsafe variants. teehee
-
-    lookup' :: forall k v. Ord k => k -> Map k v -> v
-    lookup' k m = unsafePartial $ fromJust (Map.lookup k m)
-
-    maximum' :: forall f a. Foldable f => Ord a => f a -> a
-    maximum' xs = unsafePartial $ fromJust $ maximum xs
-
-    minimum' :: forall f a. Foldable f => Ord a => f a -> a
-    minimum' xs = unsafePartial $ fromJust $ minimum xs
 
     -- Stage 1. Formative arrangements
     --          Y-positions are assigned as a trivial function of tier
@@ -70,7 +58,7 @@ arrange getId getDeps getTime getDims =
     --          Then, divide the x-axis among these nodes in proportion to their weight.
     --          Finally, place each node halfway between the center of its portion and
     --          the closest x-position in its portion to its ideal x-position.
-    stage_1'formativeArrangement :: List node -> Map id Vec2
+    stage_1'formativeArrangement :: Partial => List node -> Map id Vec2
     stage_1'formativeArrangement allNodes = go 0 Map.empty
       where
         tierByNode = calcTiers getId getDeps allNodes
@@ -79,10 +67,10 @@ arrange getId getDeps getTime getDims =
 
         -- The weight of a node is the maximum number of nodes in
         -- any descendant tier, or 1 if there are no descendant tiers
-        weight :: node -> Int
+        weight :: Partial => node -> Int
         weight node = allTiers
-                    # List.filter (_ > (tierByNode # lookup' node))
-                    # map (\tier -> nodesByTier # lookup' tier # length)
+                    # List.filter (_ > (tierByNode # Map.lookup node # fromJust))
+                    # map (\tier -> nodesByTier # Map.lookup tier # fromJust # length)
                     # maximum
                     # fromMaybe 1
 
@@ -91,10 +79,10 @@ arrange getId getDeps getTime getDims =
             Nothing -> soFar
             Just these -> go (tier + 1) (soFar `Map.union` arrangeTierBasedOnArrangementSoFar tier soFar these)
 
-        arrangeTierBasedOnArrangementSoFar :: Int -> Map id Vec2 -> List node -> Map id Vec2
+        arrangeTierBasedOnArrangementSoFar :: Partial => Int -> Map id Vec2 -> List node -> Map id Vec2
         arrangeTierBasedOnArrangementSoFar tier soFar nodes =
           let
-            calcIdealX = \node -> node # getDeps # map (\dep -> lookup' dep soFar # getX) # average # fromMaybe 0.5
+            calcIdealX = \node -> node # getDeps # map (\dep -> Map.lookup dep soFar # fromJust # getX) # average # fromMaybe 0.5
             nodesOrdered = nodes # List.sortBy (compare `on` \node -> calcIdealX node /\ getTime node)
             calcPortion = \node -> Int.toNumber (weight node) / Int.toNumber (sum $ map weight nodes)
             nodePlacements =
@@ -114,14 +102,14 @@ arrange getId getDeps getTime getDims =
     --          Stage 1 assigned x- and y- positions with concern only to relative values.
     --          Now we will fix this, by scaling the given values into a "reasonable" range.
     --          Note that the x- and y- axes need to be normalized independently.
-    stage_2'normalization :: Map id Vec2 -> Map id Vec2
+    stage_2'normalization :: Partial => Map id Vec2 -> Map id Vec2
     stage_2'normalization placements =
       if Map.isEmpty placements then Map.empty else
       let
         xs = placements # map getX
         ys = placements # map getY
-        xRange = maximum' xs - minimum' xs
-        yRange = maximum' ys - minimum' ys
+        xRange = fromJust (maximum xs) - fromJust (minimum xs)
+        yRange = fromJust (maximum ys) - fromJust (minimum ys)
         nodeCount = placements # Map.keys # length
         targetXRange = Int.toNumber $ nodeCount * 80
         targetYRange = Int.toNumber $ nodeCount * 50
