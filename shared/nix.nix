4422547a0a484737b4f3e97410c4c893d2d130fc
Maynard <me@maynards.site>
2022-10-16 12:26:19 -0700

    faster nixpkgs evaluation

--- shared/nix.nix
+++ shared/nix.nix
@@ -3,11 +3,13 @@
 let
 
 pkgs =
-  import
-    (builtins.fetchGit
-      { url = "https://github.com/NixOS/nixpkgs";
-        rev = "37cc765b36b0f74e4f18800e466496bacb366a35";
-      }) { };
+  let
+    rev = "37cc765b36b0f74e4f18800e466496bacb366a35";
+    src =
+      builtins.fetchTarball
+        "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  in
+    import src {};
 
 get-flake =
   import
